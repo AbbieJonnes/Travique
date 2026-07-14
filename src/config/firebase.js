11bec5bea63c6733fe89a63e6a08ab9f3a63fcbf
abbiejonnes <abigaelmwangi534@gmail.com>
2026-07-14 23:52:31 +0300

    fix: update the firebase.js by adding the api key from the firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCEsbh-tna3rKso1ux4jVAF2szXs1mIxo8",
+  authDomain: "travique-66263.firebaseapp.com",
+  projectId: "travique-66263",
+  storageBucket: "travique-66263.firebasestorage.app",
+  messagingSenderId: "105169727097",
+  appId: "1:105169727097:web:6b52c8eb1abe3643c0aea8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
