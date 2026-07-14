85f3a5e34fdca0354b1317c031a8b074d4304e22
abbiejonnes <abigaelmwangi534@gmail.com>
2026-07-15 00:16:18 +0300

    fix: update the firebase.js by adding the firestore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCEsbh-tna3rKso1ux4jVAF2szXs1mIxo8",
-  authDomain: "travique-66263.firebaseapp.com",
-  projectId: "travique-66263",
-  storageBucket: "travique-66263.firebasestorage.app",
-  messagingSenderId: "105169727097",
-  appId: "1:105169727097:web:6b52c8eb1abe3643c0aea8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
