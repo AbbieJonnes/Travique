21adba897bc09a977fd9a71dee0769a757965b45
abbiejonnes <abigaelmwangi534@gmail.com>
2026-07-15 22:19:20 +0300

    fix:update the app.jsx by importing the register and creating it's route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-//import Register from "./pages/Register";
+import Register from "./pages/Register";
 //import Login from "./pages/Login";
 //import Dashboard from "./pages/Dashboard";
 //import Destinations from "./pages/Destinations";
@@ -18,8 +18,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
 
-        {/* <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* <Route path="/login" element={<Login />} />
 
         <Route path="/dashboard" element={<Dashboard />} />
 
