929ee2881b56e3d35420021113e187b14db56bd8
abbiejonnes <abigaelmwangi534@gmail.com>
2026-07-15 10:36:12 +0300

    fix: update the app.jsx by adding the home import and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
+//import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
@@ -16,7 +16,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
 
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
