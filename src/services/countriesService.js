581222a3d2aa85db7392425ac1d4a12a0fc193fd
abbiejonnes <abigaelmwangi534@gmail.com>
2026-07-16 22:21:23 +0300

    update: update the countriesServices by adding the API key

---
+++ src/services/countriesService.js
@@ -0,0 +1,20 @@
+const BASE_URL = "https://restcountries.com/v3.1/name";
+
+export async function getCountryDetails(country) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/${country}?fullText=true`
+    );
+
+    if (!response.ok) {
+      throw new Error("Country not found.");
+    }
+
+    const data = await response.json();
+
+    return data[0];
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
