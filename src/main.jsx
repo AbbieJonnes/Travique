bd7904d96a3abacb84cdeb0baea7b27495cd90cd
abbiejonnes <abigaelmwangi534@gmail.com>
2026-07-17 07:26:51 +0300

    fix:update the main.jsx by having the favoritesprovider wrap after bookingprovider

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext';
 import { BookingProvider } from './context/BookingContext';
-import { FavoritesProvider } from './context/FavouritesContext';
+import { FavoritesProvider } from './context/FavoritesContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <BookingProvider>
-      <FavoritesProvider/>
+      <FavoritesProvider>
       <App />
+      </FavoritesProvider>
       </BookingProvider>
     </AuthProvider>
   </StrictMode>,
