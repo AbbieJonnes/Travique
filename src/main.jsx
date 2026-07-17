cd9eaef910986208b241238c541f2a72a716c03a
abbiejonnes <abigaelmwangi534@gmail.com>
2026-07-17 07:03:37 +0300

    fix:update the main.jsx by adding the favorites import

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext';
 import { BookingProvider } from './context/BookingContext';
+import { FavoritesProvider } from './context/FavouritesContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <BookingProvider>
+      <FavoritesProvider/>
       <App />
       </BookingProvider>
     </AuthProvider>
