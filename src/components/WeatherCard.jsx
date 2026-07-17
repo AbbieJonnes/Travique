92d691c4e536aab6cedb3e12b57c83a03cd03986
abbiejonnes <abigaelmwangi534@gmail.com>
2026-07-17 10:23:07 +0300

    fix:update the weathercard by removing the useeffect

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,30 +1,5 @@
-import { useEffect, useState } from "react";
-import { getWeather } from "../services/weatherService";
-
-function WeatherCard({ city }) {
-  const [weather, setWeather] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    async function fetchWeather() {
-      try {
-        const data = await getWeather(city);
-        setWeather(data);
-      } catch (err) {
-       console.error(err);
-        setError("Unable to load weather.");
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    if (city) {
-      fetchWeather();
-    }
-  }, [city]);
-
-  if (loading) {
+function WeatherCard({ weather }) {
+  if (!weather) {
     return (
       <div className="bg-white rounded-2xl shadow-md p-6">
         Loading weather...
@@ -32,30 +7,19 @@ function WeatherCard({ city }) {
     );
   }
 
-  if (error) {
-    return (
-      <div className="bg-red-100 text-red-600 rounded-2xl p-6">
-        {error}
-      </div>
-    );
-  }
-
   return (
     <div className="bg-white rounded-2xl shadow-lg p-6">
-
       <h2 className="text-2xl font-bold mb-4">
         Current Weather
       </h2>
 
       <div className="flex items-center gap-4">
-
         <img
           src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
           alt={weather.weather[0].description}
         />
 
         <div>
-
           <h3 className="text-xl font-semibold">
             {weather.name}
           </h3>
@@ -67,13 +31,10 @@ function WeatherCard({ city }) {
           <p className="text-4xl font-bold mt-2">
             {Math.round(weather.main.temp)}°C
           </p>
-
         </div>
-
       </div>
 
       <div className="grid grid-cols-2 gap-4 mt-6">
-
         <div>
           <p className="text-gray-500">Humidity</p>
           <p className="font-semibold">
@@ -87,9 +48,7 @@ function WeatherCard({ city }) {
             {weather.wind.speed} m/s
           </p>
         </div>
-
       </div>
-
     </div>
   );
 }
